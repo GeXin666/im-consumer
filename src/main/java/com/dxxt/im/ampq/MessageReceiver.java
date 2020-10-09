0bd50af523d2428df9914d24aac9f255d1aa6e37
Ge <183615215@qq.com>
2020-10-09 17:50:22 +0800

    first commit

--- src/main/java/com/dxxt/im/ampq/MessageReceiver.java
+++ src/main/java/com/dxxt/im/ampq/MessageReceiver.java
@@ -1,9 +1,11 @@
 package com.dxxt.im.ampq;
 
+import com.dxxt.im.util.JedisUtil;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.type.TypeFactory;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +21,9 @@ public class MessageReceiver {
     @Autowired
     private ObjectMapper jsonMapper;
 
+    @Autowired
+    private AmqpTemplate rabbitMq;
+
     @RabbitHandler
     public void process(String message) {
         log.info("Receive rabbitmq message: {}", message);
@@ -26,6 +31,17 @@ public class MessageReceiver {
         JavaType javaType = TypeFactory.defaultInstance().constructType(LinkedHashMap.class);
         try {
             LinkedHashMap<String, Object> dataMap = jsonMapper.readValue(message,javaType);
+            String clientId = (String) dataMap.get("clientId");
+            String strMsg = (String) dataMap.get("message");
+
+            //去Redis查询routingKey
+            String routingKey = JedisUtil.getString(clientId);
+            //用户在线-转发消息
+            if(routingKey != null) {
+                rabbitMq.convertAndSend(AmpqConfig.exchangeName, routingKey, message);
+            } else {
+                log.info("用户不在线-存储离线消息 userId: {}  message: {}", clientId, strMsg);
+            }
         } catch (Exception e) {
             log.error("Parse rabbitmq message error: {}" , message, e);
         }
